eef99509895fba5985a04e686cc07781d51376f5
Vinicius Fagundes <mvlacerda@gmail.com>
2015-11-27 21:55:06 -0200

    Add validators for Brazilian Registration Numbers (CPF and CNPJ)

---
+++ src/Validator/Cnpj.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @license http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2015 Vinicius Fagundes
+ */
+
+namespace Zff\Base\Validator;
+
+/**
+ * @reference http://www.schoolofnet.com/2015/04/como-validar-cpf-e-cnpj-usando-zend-framework-2/
+ * 
+ * Validates the Brazilian Number of Corporate Registration (CNPJ).
+ */
+class Cnpj extends AbstractCgc
+{
+    /**
+     * Tamanho do Campo
+     * @var int
+     */
+    protected $size = 14;
+
+    /**
+     * Modificadores de Dígitos
+     * @var array
+     */
+    protected $modifiers = [
+        [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2],
+        [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    ];
+
+}
---
+++ src/Validator/Cpf.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @license http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2015 Vinicius Fagundes
+ */
+
+namespace Zff\Base\Validator;
+
+/**
+ * @reference http://www.schoolofnet.com/2015/04/como-validar-cpf-e-cnpj-usando-zend-framework-2/
+ *
+ * Validates the Brazilian Taxpayer Registration (CPF).
+ */
+class Cpf extends AbstractCgc
+{
+    /**
+     * Tamanho do Campo
+     * @var int
+     */
+    protected $size = 11;
+
+    /**
+     * Modificadores de Dígitos
+     * @var array
+     */
+    protected $modifiers = [
+        [10, 9, 8, 7, 6, 5, 4, 3, 2],
+        [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+    ];
+
+}
